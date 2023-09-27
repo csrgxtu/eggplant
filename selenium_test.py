4c765ad03c19cba2bee6f41742c548785840bbab
minyakonga <minyakonga@gmail.com>
2023-09-27 20:00:23 +0800

    use selenium render the douyin webapge and get the real video cdn link and download it

---
+++ selenium_test.py
@@ -0,0 +1,35 @@
+import time
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# url = "https://v.douyin.com/ievRk7uf/"
+# url = "https://v.douyin.com/ievfYXSy/"
+url = "https://v.douyin.com/ievfVbTR/"
+
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(5)
+
+with open('./douyin.html', 'w') as f:
+    f.write(driver.page_source)
+
+if '<source' not in driver.page_source:
+    print('Not Found')
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+sources = soup.find_all('source')
+video_link = "https:" + sources[-1].attrs['src']
+print(f'Video Link: {video_link}')
+
+driver.get(video_link)
+time.sleep(3)
+cdn_link = driver.current_url
+driver.close()
+print(f'After 302: {cdn_link}')
+
+res = requests.get(cdn_link)
+print(f'{res.status_code}, {res.headers}')
+with open('./test.mp4', 'wb') as f:
+    f.write(res.content)
+print(f'Done, saved video into file test.mp4')
